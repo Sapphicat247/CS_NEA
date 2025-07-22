a321cdc034cd8b130f2f5780dbf6261559be9e4e
Sapphicat247 <jacobzassbangham@gmail.com>
2025-07-22 20:45:10 +0100

    create flag enum for directions and structs for hexes, edges, and verteces

--- src/catan.py
+++ src/catan.py
@@ -1,5 +1,17 @@
-from enum import Enum
-from dataclasses import dataclass
+from enum import Enum, Flag
+from dataclasses import dataclass, field, MISSING
+
+class Direction(Flag):
+    N  = 2**0
+    NE = 2**1
+    E  = 2**2
+    SE = 2**3
+    S  = 2**4
+    SW = 2**5
+    W  = 2**6
+    NW = 2**7
+
+# MARK: board contents
 
 class Colour(Enum):
     NONE = 0 # only used as placeholder
@@ -26,3 +38,35 @@ class Resource(Enum):
     BRICK = 3
     ORE = 4
     GRAIN = 5
+
+@dataclass
+class Port:
+    resource: Resource
+    direction: Direction # for drawing on screen
+
+# MARK: board construction
+
+# "pointers" to objects are dictionaries with set keys
+
+@dataclass
+class Vertex:
+    structure: Structure = field(default_factory=Structure)
+    
+    edges: dict = field(default_factory = dict)
+
+@dataclass
+class Edge:
+    port: Port # compare with MISSING to check if port exists: https://stackoverflow.com/questions/53589794/pythonic-way-to-check-if-a-dataclass-field-has-a-default-value
+    
+    structure: Structure = field(default_factory=Structure)
+    
+    verts: dict = field(default_factory = dict)
+
+@dataclass
+class Hex:
+    resource: Resource = Resource.DESERT
+    diceValue: int = 0
+    hasRobber: bool = False
+    
+    hexes: dict = field(default_factory = dict)
+    verts: dict = field(default_factory = dict)
