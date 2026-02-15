b4bb7849f088f375374bfe6f7b4052e955ae3470
jacob <jacobzassbangham@gmail.com>
2026-02-15 15:47:07 +0000

    update testing script

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 for i in {1..200}; do
     result=$(python3 ./AI_benchmarking.py)
     echo $result
-    echo $result | grep -E -o "[0-9]+\.[0-9]+" >> results.txt
+    echo $result | grep -E -o "[0-9]+\.[0-9]+" >> timing.txt
+    echo $result | grep -E -o "RED|ORAGE|BLUE|WHITE" >> winners.txt
 done
